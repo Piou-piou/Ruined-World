5da166abaf4a97c6b58c2ab0a9b264bf5905934b
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-10-31 16:57:38 +0100

    reput print_r

--- index.php
+++ index.php
@@ -95,9 +95,9 @@
 				$twig = new Twig_Environment($loader);
 
 				$page = $router_module->getPage();
-				/*echo("<pre>");
+				echo("<pre>");
 				print_r($arr);
-				echo("</pre>");*/
+				echo("</pre>");
 			}
 			else {
 				$contenu->getContenuPage();
