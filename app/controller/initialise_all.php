611535bf9ee26bdbd3889f9c864f614be81b1c89
Piou-piou <pilloud.anthony@gmail.com>
2016-11-22 19:27:54 +0100

    to see table for build building

--- app/controller/initialise_all.php
+++ app/controller/initialise_all.php
@@ -1,3 +1,19 @@
 <?php
 	$gestion_module = new \core\modules\GestionModule();
 	$gestion_module->getListeModuleActiver();
+
+
+	/*$tab = [
+		[
+			"centre de commandement",
+			"centre_commandement",
+			5
+		],
+		[
+			"entrepot",
+			"entrepot",
+			5
+		]
+	];
+
+	echo(serialize($tab));*/
