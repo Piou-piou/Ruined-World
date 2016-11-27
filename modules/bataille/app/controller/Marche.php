7a940d24beff6188f2b42c26c4187279326060f4
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-11-27 12:39:37 +0100

    adding ressources delivery and calcute return time

--- modules/bataille/app/controller/Marche.php
+++ modules/bataille/app/controller/Marche.php
@@ -7,6 +7,7 @@
 	class Marche {
 		private $id_base_dest;
 		private $aller;
+		private $ressources;
 		private $date_arrivee;
 		private $id_marche_transport;
 		private $duree_restante_trajet;
@@ -26,13 +27,14 @@
 				foreach ($query as $obj) {
 					$this->id_base_dest = $obj->ID_base_dest;
 					$this->aller = $obj->aller;
+					$this->ressources = $obj->ressources;
 					$this->date_arrivee = $obj->date_arrivee;
 					$this->id_marche_transport = $obj->ID_marche_transport;
 
 					$this->getTransportArrive();
 
 					//si c'est sur l'allé
-					if ($obj->aller == 1) {
+					if ($this->aller == 1) {
 						$marche["aller"][] = [
 							"id_marche_transport" => $obj->ID_marche_transport,
 							"date_arrivee" => $this->duree_restante_trajet,
@@ -56,6 +58,12 @@
 
 
 		//-------------------------- GETTER ----------------------------------------------------------------------------//
+		/**
+		 * fonction qui permet de savoir si un transport est arrivé à la base de destination
+		 * +si arrivé appel la fonction pour ajouter les ressources et passe le trajet sur le retour
+		 * ou en trajet fini suivant le temps de la date d'aujourd'hui et la date à laquelle le
+		 * trajet aurait du revenir
+		 */
 		private function getTransportArrive() {
 			$today = new \DateTime();
 			$today = $today->getTimestamp();
@@ -64,26 +72,23 @@
 			echo $this->date_arrivee."<br>";
 
 			//on test si déja arrivé à destination
-			if (($this->aller == 1) && (($this->date_arrivee-$today) <= 0)) {echo("dgd");
+			if (($this->aller == 1) && (($this->date_arrivee-$today) <= 0)) {
 				$this->setLivrerRessource();
 
 				//on calcul la date d'arrivée du retour
 				$date_retour = Bataille::getDureeTrajet($this->id_base_dest)+$this->date_arrivee;
 
-				echo "date retour = ".$date_retour;
-
-				//si le retour du trajet est également arrivé on finit le transport
+				//si le retour du trajet est également arrivé on finit le transport sinon on le place sur le retour
 				if ($date_retour < $today) {
 					$this->setTerminerTransport();
 				}
 				else {
-
+					$this->setTrajetRetour($date_retour);
+					$this->duree_restante_trajet = $date_retour-$today;
 				}
-
-
 			}
 			else if (($this->aller == 0) && (($this->date_arrivee-$today) <= 0)) {
-
+				$this->setTerminerTransport();
 			}
 			else {
 				$this->duree_restante_trajet = $this->date_arrivee-$today;
@@ -98,7 +103,25 @@
 		 * fonction qui permet d'ajouter les ressources à la base destinatrice du transport
 		 */
 		private function setLivrerRessource() {
+			$ressource = new Ressource($this->id_base_dest);
 
+			$ressource_transport = unserialize($this->ressources);
+
+			$ressource->setUpdateRessource($ressource_transport['eau'], $ressource_transport['electricite'], $ressource_transport['fer'], $ressource_transport['fuel'], $ressource_transport['nourriture'], "+");
+		}
+
+		/**
+		 * fonction qui place le trajet en retour
+		 */
+		private function setTrajetRetour($date_retour) {
+			$dbc = App::getDb();
+
+			$dbc->update("ressources", 0)
+				->update("aller", 0)
+				->update("date_arrivee", $date_retour)
+				->from("_bataille_marche_transport")
+				->where("ID_marche_transport", "=", $this->id_marche_transport)
+				->set();
 		}
 
 		/**
