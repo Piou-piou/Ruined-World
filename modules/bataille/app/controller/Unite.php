861c7016a6b7c0c1f5391995c289054d98799c27
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-12-06 18:54:38 +0100

    init unit class

--- modules/bataille/app/controller/Unite.php
+++ modules/bataille/app/controller/Unite.php
@@ -14,6 +14,18 @@
 		
 		
 		//-------------------------- GETTER ----------------------------------------------------------------------------//
+		public function getInfosUnite($unite, $niveau) {
+			$dbc1 = Bataille::getDb();
+
+			$query = $dbc1->select()->from("unites")->where("nom", "=", $unite)->get();
+
+			if ((is_array($query)) && (count($query) == 1)) {
+
+			}
+			else {
+				return false;
+			}
+		}
 		//-------------------------- END GETTER ----------------------------------------------------------------------------//
 		
 		
