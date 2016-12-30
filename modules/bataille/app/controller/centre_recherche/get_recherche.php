7c57b844cabd1eeafed0cd758f9c8c4f4553996b
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-12-30 17:31:32 +0100

    now when we research anything, in popup of centre_recherche we can't do anything while research is not terminated

--- modules/bataille/app/controller/centre_recherche/get_recherche.php
+++ modules/bataille/app/controller/centre_recherche/get_recherche.php
@@ -1,4 +1,6 @@
 <?php
 	$centre_recherche = new \modules\bataille\app\controller\CentreRecherche();
 
+	\modules\bataille\app\controller\Bataille::getCentreRecherche()->getRecherche();
+
 	$arr = \modules\bataille\app\controller\Bataille::getValues();
