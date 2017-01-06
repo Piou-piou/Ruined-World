0c45626b1199605cd2849697fb17c9ae422889eb
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-06 13:05:43 +0100

    adding new map system

---
+++ modules/bataille/app/views/js/map-game.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $map = $(".map-game");
+    width = $map.width();
+    height = $map.height();
+    rows = Math.round(height/20);
+    columns = Math.round(width/20);
+
+    for (i=0 ; i<rows ; i++) {
+        for (j=0 ; j<columns ; j++) {
+            pleft = j*20;
+            ptop = i*20;
+
+            $map.append("<div class='case' id='case-"+pleft+"-"+ptop+"' style='left:"+pleft+"px;top:"+ptop+"px;'></div>");
+        }
+    }
+
+
+    $(".map-game .case").on("click", function() {
+        $(this).css("background-color", "red");
+    })
+})
