078e2c691de620de2270a4eeedcbbe4b8dabdc26
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-11 14:40:41 +0100

    delete emplacement var

--- modules/bataille/app/controller/batiment/construire.php
+++ modules/bataille/app/controller/batiment/construire.php
@@ -1,6 +1,5 @@
 <?php
 	$nom_batiment = $_POST['nom_batiment'];
 	$nom_batiment_sql = $_POST['nom_batiment_sql'];
-	$emplacement = $_POST['emplacement'];
 
-	\modules\bataille\app\controller\Bataille::getBatiment()->setCommencerConstruireBatiment($nom_batiment, $nom_batiment_sql, $emplacement);
+	\modules\bataille\app\controller\Bataille::getBatiment()->setCommencerConstruireBatiment($nom_batiment, $nom_batiment_sql);
