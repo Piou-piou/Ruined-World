d16fb42a7f6232adabf8083086f8a925d775627f
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-15 12:35:53 +0100

    show error when try to build a building and let popup open

--- modules/bataille/app/controller/Batiment.php
+++ modules/bataille/app/controller/Batiment.php
@@ -676,6 +676,8 @@
 
 					//on retire les ressources de la base
 					Bataille::getRessource()->setUpdateRessource($ressource_construction[2], $ressource_construction[3], $ressource_construction[0], $ressource_construction[1], 0, "-");
+				
+					return "ok";
 				}
 			}
 			else {
--- modules/bataille/app/controller/batiment/construire.php
+++ modules/bataille/app/controller/batiment/construire.php
@@ -2,4 +2,6 @@
 	$nom_batiment = $_POST['nom_batiment'];
 	$nom_batiment_sql = $_POST['nom_batiment_sql'];
 
-	\modules\bataille\app\controller\Bataille::getBatiment()->setCommencerConstruireBatiment($nom_batiment, $nom_batiment_sql);
+	if (\modules\bataille\app\controller\Bataille::getBatiment()->setCommencerConstruireBatiment($nom_batiment, $nom_batiment_sql) == false) {
+		\core\HTML\flashmessage\FlashMessage::getFlash();
+	}
