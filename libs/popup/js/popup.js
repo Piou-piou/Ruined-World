57d542e55aa3948aa5e1485610ea86a5ac2fa17d
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-18 08:20:48 +0100

    adding overflow hidden on body when open the popup

--- libs/popup/js/popup.js
+++ libs/popup/js/popup.js
@@ -7,15 +7,19 @@ function OpenSupprimerPopup(lien, id_popup) {
 $(document).ready(function() {
     $(".popup .fermer").click(function() {
         $(".popup").removeClass("visible");
+        $("body").css("overflow-y", "auto");
     });
     $(".popup .annuler").click(function() {
         $(".popup").removeClass("visible");
         $(".popup").find("a.valider").attr("href", "");
+        $("body").css("overflow-y", "auto");
     });
 
     //popup qui s'ouvre pour valider suppression article bloc
     $(".open-popup").click(function(e) {
         e.preventDefault();
+        
+        $("body").css("overflow-y", "hidden");
 
         OpenSupprimerPopup($(this).attr("href"), $(this).attr("popup"));
     });
