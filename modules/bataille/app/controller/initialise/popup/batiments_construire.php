174aee1a9084387b07a993170a65e19eaf21c686
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-18 17:21:06 +0100

    delete because unused with new building system

--- modules/bataille/app/controller/initialise/popup/batiments_construire.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*require_once(MODULEROOT."bataille/app/controller/initialise/test_connexion.php");
-
-	modules\bataille\app\controller\Bataille::getIdBase();
-
-	modules\bataille\app\controller\Bataille::getBatiment()->getBatimentAConstruire();
-
-	$arr = \modules\bataille\app\controller\Bataille::getValues();*/
