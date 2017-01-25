b12be7f31e8433e2284e2e097d61abc0fc3a7589
Piou-piou <pilloud.anthony@gmail.com>
2017-01-25 21:18:10 +0100

    hotfix header location not placed in good file

--- modules/bataille/app/controller/missions/get_all_missions.php
+++ modules/bataille/app/controller/missions/get_all_missions.php
@@ -4,5 +4,3 @@
 	\modules\bataille\app\controller\Bataille::getUnite()->getAllUnites();
 	
 	$arr = \modules\bataille\app\controller\Bataille::getValues();
-	
-	header("location:".WEBROOT."bataille");
--- modules/bataille/app/controller/missions/lancer_mission.php
+++ modules/bataille/app/controller/missions/lancer_mission.php
@@ -1,2 +1,4 @@
 <?php
 	\modules\bataille\app\controller\Bataille::getMissionsAleatoire()->setCommencerMission($_POST["id_mission"], $_POST["nombre_unite"], $_POST["nom_unite"], $_POST["type_unite"]);
+	
+	header("location:".WEBROOT."bataille");
