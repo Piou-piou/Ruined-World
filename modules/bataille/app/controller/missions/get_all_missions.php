da4356e6c1e361bc0f933f715972279894ff6807
Piou-piou <pilloud.anthony@gmail.com>
2017-01-25 18:22:23 +0100

    add form to send units

--- modules/bataille/app/controller/missions/get_all_missions.php
+++ modules/bataille/app/controller/missions/get_all_missions.php
@@ -1,4 +1,6 @@
 <?php
 	$missions = \modules\bataille\app\controller\Bataille::getMissionsAleatoire()->getMissions("nourriture");
 	
+	\modules\bataille\app\controller\Bataille::getUnite()->getAllUnites();
+	
 	$arr = \modules\bataille\app\controller\Bataille::getValues();
