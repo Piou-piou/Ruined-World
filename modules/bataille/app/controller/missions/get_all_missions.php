2edd40df4aac1ff03af9f771d1576185918ad9c9
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-28 19:03:46 +0100

    add live seeing adding ressource after mission (no including death of some units)

--- modules/bataille/app/controller/missions/get_all_missions.php
+++ modules/bataille/app/controller/missions/get_all_missions.php
@@ -3,4 +3,13 @@
 	
 	\modules\bataille\app\controller\Bataille::getUnite()->getAllUnites();
 	
+	/*$stockage = [
+		"max_entrepot" => \modules\bataille\app\controller\Bataille::getBatiment()->getStockage(),
+		"max_grenier" => \modules\bataille\app\controller\Bataille::getBatiment()->getStockage("grenier")
+	];*/
+	
+	\modules\bataille\app\controller\Bataille::getBase()->getMaBase();
+	
 	$arr = \modules\bataille\app\controller\Bataille::getValues();
+	
+	/*$arr = array_merge($arr, $stockage);*/
