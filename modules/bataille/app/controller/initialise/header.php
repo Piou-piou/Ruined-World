7733db0e702b0ba847aeca5b0cf533d954b3d83a
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-28 17:23:57 +0100

    now at the end of random mission it create a report of the mission

--- modules/bataille/app/controller/initialise/header.php
+++ modules/bataille/app/controller/initialise/header.php
@@ -8,6 +8,8 @@
 	
 	Bataille::getNation();
 	
+	Bataille::getMissionsAleatoire()->setTerminerMissions();
+	
 	//\modules\bataille\app\controller\GenerationRapport::setGenererRapport("rapport de mission", "toto", "mission");
 	
 	$arr = Bataille::getValues();
