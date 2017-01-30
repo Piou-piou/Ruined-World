b11ca57842dc37a994c11d051a7339263f50c939
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-30 14:37:33 +0100

    update header for base changment now it is by a form method post

--- modules/bataille/app/controller/initialise/test_connexion.php
+++ modules/bataille/app/controller/initialise/test_connexion.php
@@ -2,10 +2,13 @@
 	\core\auth\Connexion::setConnexion(WEBROOT."bataille/login");
 
 	\core\auth\Connexion::setObgConnecte(WEBROOT."bataille/login");
-
+	
 	//test si on a une base dans l'url
 	if (isset($_GET['base'])) {
 		$_SESSION['id_base'] = $_GET['base'];
+	}
+	else if (isset($_POST['base'])) {
+		$_SESSION['id_base'] = $_POST['base'];
 	}
 	else {
 		$_SESSION['id_base'] = \modules\bataille\app\controller\Bataille::getFirstBase();
