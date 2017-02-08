b7d009181eef68c184dc0e16a398940a661ddf32
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-02-08 16:48:03 +0100

    prepare view to create new group of units

--- modules/bataille/app/controller/unites/initialise_groupe.php
+++ modules/bataille/app/controller/unites/initialise_groupe.php
@@ -3,4 +3,6 @@
 	$groupe = new \modules\bataille\app\controller\GroupeUnite();
 	$groupe->getAllGroupeUnite();
 	
+	\modules\bataille\app\controller\Bataille::getUnite()->getAllUnites();
+	
 	$arr = \modules\bataille\app\controller\Bataille::getValues();
