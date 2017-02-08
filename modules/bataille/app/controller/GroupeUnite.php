504dca172be04144b813ecefa263fad53dce6d5b
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-02-08 16:56:47 +0100

    prepare function to create a group for the moment just return false if a group with this name already exist

--- modules/bataille/app/controller/GroupeUnite.php
+++ modules/bataille/app/controller/GroupeUnite.php
@@ -3,6 +3,7 @@
 	
 	
 	use core\App;
+	use core\HTML\flashmessage\FlashMessage;
 	
 	class GroupeUnite extends Unite {
 		
@@ -34,9 +35,43 @@
 				Bataille::setValues(["groupe_unites" => $groupe]);
 			}
 		}
+		
+		/**
+		 * @param $nom_groupe
+		 * @return bool
+		 * fonction qui test si un groupe existe déjà ou pas dans cette base
+		 */
+		private function getTestGroupExist($nom_groupe) {
+			$dbc = App::getDb();
+			
+			$query = $dbc->select("ID_groupe_unite")
+				->where("nom_groupe", "=", $nom_groupe, "AND")
+				->where("ID_base", "=", Bataille::getIdBase())
+				->get();
+			
+			if (count($query) > 0) {
+				return false;
+			}
+			
+			return true;
+		}
 		//-------------------------- END GETTER ----------------------------------------------------------------------------//
 		
 		
 		//-------------------------- SETTER ----------------------------------------------------------------------------//
+		/**
+		 * @param $nombre_unite
+		 * @param $nom_unite
+		 * @param $type_unite
+		 * @param $nom_groupe
+		 * @return bool
+		 * fonction qui permet de créer un groupe
+		 */
+		public function setCreerGroupe($nombre_unite, $nom_unite, $type_unite, $nom_groupe) {
+			if ($this->getTestGroupExist($nom_groupe) == false) {
+				FlashMessage::setFlash("Un groupe portant ce nom existe déjà, merci d'en choisir un autre !");
+				return false;
+			}
+		}
 		//-------------------------- END SETTER ----------------------------------------------------------------------------//    
 	}
