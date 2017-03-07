7d861bb7dfcf19a458f49cc213a8cc770eaef235
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-03-07 16:19:16 +0100

    add js for login to unable login if account is under holidays mod less than 48 hours

---
+++ modules/bataille/app/controller/auth/login_ajax.php
@@ -0,0 +1,17 @@
+<?php
+	$query = \core\App::getDb()->select("ID_identite")->from("identite")->where("pseudo", "=", $_POST['pseudo'])->get();
+	
+	if (count($query) == 1) {
+		foreach ($query as $obj) {
+			$_SESSION['idlogin'.CLEF_SITE] = $obj->ID_identite;
+		}
+	}
+	
+	$profil = new \modules\bataille\app\controller\Profil();
+	
+	if ($profil->getVacances() == false) {
+		echo("ok");
+	}
+	else {
+		\core\HTML\flashmessage\FlashMessage::getFlash();
+	}
